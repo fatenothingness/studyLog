f14f52faa7dd9c88223dc78df468b595320e05fa
张佑甄 <zhangyouzhen@vivo.com>
2021-04-09 10:56:23 +0800

    每日一题

--- main/java/OneTopicEveryday.java
+++ main/java/OneTopicEveryday.java
@@ -1,7 +1,4 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 public class OneTopicEveryday {
 
@@ -215,4 +212,114 @@ public class OneTopicEveryday {
         return false;
     }
 
+    /**
+     *90. 子集 II
+     * 给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+     * 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+     */
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        // 首先排序，让相同的两个元素排到一起去，便于去重
+        Arrays.sort(nums);
+        int n = nums.length;
+        // 使用 visited 数组来记录哪一个元素在当前路径中被使用了
+        boolean[] visited = new boolean[n];
+        // 开始回溯
+        backtrace(nums, 0, false, n,ans,path);
+        return ans;
+    }
+
+    private void backtrace(int[] nums, int start, boolean visited, int n,List<List<Integer>> ans,List<Integer> path) {
+        // 首先加入当前路径
+        ans.add(new ArrayList<>(path));
+        // 从 start 开始遍历每一个元素，尝试加入路径中
+        for (int i = start; i < n; ++i) {
+            // 如果当前元素和前一个元素相同，而且前一个元素没有被访问，说明前一个相同的元素在当前层已经被用过了
+            if (i > 0 && nums[i - 1] == nums[i] && visited) continue;
+            // 记录下来，用过了当前的元素
+            path.add(nums[i]); // 放到路径中
+            backtrace(nums, i + 1, true, n,ans,path); // 向下一个递归回溯
+            path.remove(path.size() - 1);
+            visited = false;
+        }
+    }
+
+    /**
+     *80. 删除有序数组中的重复项 II
+     * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 最多出现两次 ，返回删除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+     */
+    public int removeDuplicates(int[] nums) {
+        int res = 0;
+        boolean flag = true;
+        for(int i=0;i<nums.length;i++){
+            if(i==0){
+                res++;
+                continue;
+            }
+            if(nums[i]>nums[i-1]){
+                nums[res] = nums[i];
+                res++;
+                flag = true;
+            }
+            else{
+                if(flag){
+                    nums[res] = nums[i];
+                    res++;
+                    flag = false;
+                }else {
+                    continue;
+                }
+            }
+        }
+        return res;
+    }
+
+
+    /**
+     *81. 搜索旋转排序数组 II
+     * 已知存在一个按非降序排列的整数数组 nums ，数组中的值不必互不相同。
+     * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转 ，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,4,4,5,6,6,7] 在下标 5 处经旋转后可能变为 [4,5,6,6,7,0,1,2,4,4] 。
+     * 给你 旋转后 的数组 nums 和一个整数 target ，请你编写一个函数来判断给定的目标值是否存在于数组中。如果 nums 中存在这个目标值 target ，则返回 true ，否则返回 false 。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public boolean search(int[] nums, int target) {
+        int k = -1;
+        int len = nums.length;
+        //先判断旋转后短点的位置，如果找不到则说明数组的全部数都相同
+        for(int i=1;i<len;i++){
+            if(nums[i]<nums[i-1]){
+                k = i;
+            }
+        }
+        if(k==-1){
+            return target==nums[0];
+        }
+        //在断点的左边和右边都是非降序数组，左边是0 k-1 右边是k len-1 且 nums[0]>=nums[len-1]
+        if(target>nums[k-1]||target<nums[k]){
+            return false;
+        }
+        int mid;
+        int left;
+        int right;
+        //在左边
+        if(target>nums[len-1]){
+            left = 0;
+            right = k-1;
+            mid = left+right+1/2;
+            while(left<right){
+
+            }
+        }
+        //在右边
+        else if(target<nums[len-1]){
+
+        }else {
+            return true;
+        }
+        return false;
+    }
 }
